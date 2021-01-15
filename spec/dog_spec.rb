fd21f200577fa04bb6e45a96c56d7697a03bbc71
Jessica Natovich <jessicanatovich@yahoo.com>
2021-01-15 23:06:25 +0000

    Automatically backed up by Learn

--- spec/dog_spec.rb
+++ spec/dog_spec.rb
@@ -1,7 +1,8 @@
-end
+
 
   describe 'name' do
     it 'defines an instance variable @this_dogs_name' do
     it 'retrieves the value of an instance variable @this_dogs_name' do
       lassie = Dog.new
       lassie.instance_variable_set("@this_dogs_name","Lassie")
+    end 
