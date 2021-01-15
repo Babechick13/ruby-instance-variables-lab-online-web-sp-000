1c1428731c4905ab5360f346be50d5ae8db71590
Jessica Natovich <jessicanatovich@yahoo.com>
2021-01-15 23:10:38 +0000

    Automatically backed up by Learn

--- spec/dog_spec.rb
+++ spec/dog_spec.rb
@@ -12,14 +12,4 @@ describe 'Dog' do
     end
   end
 
-  describe 'name' do
-    it 'defines an instance variable @this_dogs_name' do
-    it 'retrieves the value of an instance variable @this_dogs_name' do
-      lassie = Dog.new
-      lassie.instance_variable_set("@this_dogs_name","Lassie")
-
-      expect(lassie.name).to eq("Lassie")
-    end
-  end
-end
-end 
+ 
